f5584463924efafdc694c040d1807f6f68655f00
Copo08 <98196543+Copo08@users.noreply.github.com>
2022-08-07 00:32:46 -0500

    modelo y serializador

--- universidad/estudiantes/models.py
+++ universidad/estudiantes/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+
+    name = models.CharField(max_length=60)
+    student_id = models.IntegerField()
+    adress= models.CharField(max_length=120)
+    major_interest=models.CharField(max_length=30)
+    university_interest=models.CharField(max_length=30)
+
+    class Meta:
+        db_table = 'students'
+
+    def __str__(self):
+        return self.name
